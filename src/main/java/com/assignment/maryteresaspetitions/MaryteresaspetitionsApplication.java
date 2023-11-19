088dca94e9b4b2ab722dd7b8fa9c4605391e89cf
p d <pd@gmail.com>
2023-11-19 22:38:11 +0000

    Initial commit of the project

---
+++ src/main/java/com/assignment/maryteresaspetitions/MaryteresaspetitionsApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.maryteresaspetitions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaryteresaspetitionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaryteresaspetitionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/assignment/maryteresaspetitions/MaryteresaspetitionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.maryteresaspetitions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaryteresaspetitionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
